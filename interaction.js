5c1217db1051f20ba249fdd0c7b08ade598635ef
Lagniaux Jean <34351725+jeanlagniaux@users.noreply.github.com>
2021-03-30 18:00:23 +0200

    Update interaction.js

--- interaction.js
+++ interaction.js
@@ -1,21 +1,49 @@
+function DragAndDrop(canvas, interactor) {
 
-// La création d'un Dnd requière un canvas et un interacteur.
-// L'interacteur viendra dans un second temps donc ne vous en souciez pas au départ.
-function DnD(canvas, interactor) {
-  // Définir ici les attributs de la 'classe'
+	this.initX = 0;
+	this.initY = 0; 
+	this.endX = 0;
+	this.endY = 0;
+	this.isDown = false;
 
-  // Developper les 3 fonctions gérant les événements
+  
+	this.mousedown = function(event){
+		this.initX = GetPosition(canvas, event).x;
+		this.initY = GetPosition(canvas, event).y;
+    this.endX = GetPosition(canvas, event).x;
+    this.endY =GetPosition(canvas, event).y;
+		this.isDown = true;
+    //interactor.onInteractionStart(this);
+		console.log("Mouse down : " + this.initX + " " + this.initY);
+	}.bind(this);
 
-  // Associer les fonctions précédentes aux évènements du canvas.
+  this.mouseup = function(event){
+		if (this.isDown){
+			this.isDown = false;
+			//interactor.onInteractionEnd(this);
+			console.log("Mouse up : " + this.initX + " " + this.initY + " " + this.endX + " " + this.endY);
+		}
+	}.bind(this);
+
+	this.mousemove = function(event){
+		if (this.isDown){
+			this.endX = GetPosition(canvas, event).x;
+			this.endY = GetPosition(canvas, event).y;
+      //interactor.onInteractionUpdate(this);
+			console.log("Mouvement : " + this.endX + " " + this.endY);
+		}
+	}.bind(this);
+
+  canvas.addEventListener('mousedown', this.mousedown);
+	canvas.addEventListener('mousemove', this.mousemove);
+	canvas.addEventListener('mouseup', this.mouseup);
 };
 
-
-// Place le point de l'événement evt relativement à la position du canvas.
-function getMousePosition(canvas, evt) {
+function GetPosition(canvas, event) {
   var rect = canvas.getBoundingClientRect();
   return {
-    x: evt.clientX - rect.left,
-    y: evt.clientY - rect.top
+    x: event.clientX - rect.left,
+    y: event.clientY - rect.top
   };
 };
 
