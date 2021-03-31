44da9b10418e6418dc7a946f3bbb1506624bc522
OmniCyan <43406947+OmniCyan@users.noreply.github.com>
2021-03-31 15:02:13 +0200

    V1

--- interaction.js
+++ interaction.js
@@ -1,19 +1,21 @@
 function DragAndDrop(canvas, interactor) {
 
 	this.initX = 0;
-	this.initY = 0; 
+	this.initY = 0;
 	this.endX = 0;
 	this.endY = 0;
 	this.isDown = false;
 
-  
+
 	this.mousedown = function(event){
 		this.initX = GetPosition(canvas, event).x;
 		this.initY = GetPosition(canvas, event).y;
-    	this.endX = GetPosition(canvas, event).x;
-    	this.endY =GetPosition(canvas, event).y;
+    this.endX = GetPosition(canvas, event).x;
+    this.endY =GetPosition(canvas, event).y;
 		this.isDown = true;
-    	interactor.onInteractionStart(this);
+
+		interactor.onInteractionStart(this);
+
 		console.log("Mouse down : " + this.initX + " " + this.initY);
 	}.bind(this);
 
@@ -21,7 +23,9 @@ function DragAndDrop(canvas, interactor) {
 		if (this.isDown){
 			this.endX = GetPosition(canvas, event).x;
 			this.endY = GetPosition(canvas, event).y;
-      		interactor.onInteractionUpdate(this);
+
+			interactor.onInteractionUpdate(this);
+
 			console.log("Mouvement : " + this.endX + " " + this.endY);
 		}
 	}.bind(this);
@@ -29,12 +33,14 @@ function DragAndDrop(canvas, interactor) {
   	this.mouseup = function(event){
 		if (this.isDown){
 			this.isDown = false;
+
 			interactor.onInteractionEnd(this);
+
 			console.log("Mouse up : " + this.initX + " " + this.initY + " " + this.endX + " " + this.endY);
 		}
 	}.bind(this);
 
-  	canvas.addEventListener('mousedown', this.mousedown);
+  canvas.addEventListener('mousedown', this.mousedown);
 	canvas.addEventListener('mousemove', this.mousemove);
 	canvas.addEventListener('mouseup', this.mouseup);
 };
@@ -46,6 +52,3 @@ function GetPosition(canvas, event) {
     y: event.clientY - rect.top
   };
 };
-
-
-
